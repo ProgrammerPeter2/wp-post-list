004153a24d541b0dc4209f8e8331f16497858d61
ProgrammerPeter2 <66070387+ProgrammerPeter2@users.noreply.github.com>
2023-09-21 07:55:34 +0000

    get the thumbnail url

--- PostList.php
+++ PostList.php
@@ -10,7 +10,7 @@ class PostList{
     }
 
     public function register_shortcode(){
-        add_shortcode('posts', array($this, 'post_list_shortcode'));
+        add_shortcode('post_list', array($this, 'post_list_shortcode'));
     }
 
     private function get_error_msg($msg){
@@ -23,7 +23,30 @@ class PostList{
         $category = get_category_by_slug($atts["category"]);
         if(!$category) return $this->get_error_msg("Hibás kategória slug!");
         $limit = (isset($atts["limit"])) ? intval($atts["limit"]) : 3;
-        var_dump($category);
+        // query the posts
+        $new_query = new WP_Query(
+            array(
+                'post_type'         => 'post',
+                //'posts_per_page'    => $limit,
+                'category_name'     => $category->slug;
+            )
+        );
+        $output = '<div class="post_list">';
+        if ($new_query->have_posts()) {
+            $i = 0;
+            while ($new_query->have_posts()) {
+                $new_query->the_post();
+                $postid = get_the_ID();
+				$thumb_url = get_the_post_thumbnail_url();
+                $row = <<<HTML
+                    <div class="post_item">
+                        {$thumb_url}
+                    </div>
+                HTML;
+				$output .= $row;
+            }
+        }
+		return $output."</div>";
     }
 }
 
