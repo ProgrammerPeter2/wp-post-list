2748e29230975bae8c24b8b315d8e47df7e7507d
ProgrammerPeter2 <66070387+ProgrammerPeter2@users.noreply.github.com>
2023-09-21 07:23:29 +0000

    fixed some erros

--- PostList.php
+++ PostList.php
@@ -6,11 +6,11 @@ Version: 1.0
 
 class PostList{
     public function __construct() {
-        add_action('init', array($this, 'register_shortcode'))
+        add_action('init', array($this, 'register_shortcode'));
     }
 
     public function register_shortcode(){
-        add_shortcode('posts', array($this, 'post_list_shortcode');
+        add_shortcode('posts', array($this, 'post_list_shortcode'));
     }
 
     private function get_error_msg($msg){
@@ -19,7 +19,7 @@ class PostList{
 
     public function post_list_shortcode($atts, $content, $tag){
         if(!is_array($atts)) $this->get_error_msg("Kérlek listaként add meg a paramétereit!");
-        if(!isset($atts["category"]) return $this->get_error_msg("Hiányzik a kategória!");
+        if(!isset($atts["category"])) return $this->get_error_msg("Hiányzik a kategória!");
         $category = get_category_by_slug($atts["category"]);
         if(!$category) return $this->get_error_msg("Hibás kategória slug!");
         $limit = (isset($atts["limit"])) ? intval($atts["limit"]) : 3;
